347a66776bb6564d457444eaa843ee0063dbb0cd
Delowar-maker <56905688+Delowar-maker@users.noreply.github.com>
2023-10-23 22:08:25 +0600

    onSubmit input fill
    3 way . when full from submit all value then using onSubmit event Handler

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,6 +3,18 @@ import { useState } from "react";
 
 const Register = () => {
     const [email, setEmail] = useState("")
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        // console.log(event.target.email.value);
+        // console.log(event.targer.passwor.value);
+        console.log(email, password);
+
+    };
+
+
     const handleEmailChange = (event)=>{
         console.log(event.target.value);
         setEmail(email.target.value);
@@ -14,8 +26,8 @@ const Register = () => {
     return (
         <div>
             <h1>Register Form:</h1>
-            <form onChange={handleEmailChange} action="">
-                <input type="email" name="email" id="email" />
+            <form onSubmit={handleSubmit}>
+                <input  onChange={handleEmailChange} type="email" name="email" id="email" />
                 <br />
                 <input onBlur={handlePasswordBlur} type="password" name="password"   id="password" />
                 <br />
