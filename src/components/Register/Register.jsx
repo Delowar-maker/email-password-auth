78d66350c5c2665390ba45d2d0dff60b59a876a2
Delowar-maker <56905688+Delowar-maker@users.noreply.github.com>
2023-10-23 21:46:38 +0600

    onClick Event handler
    1 way option Event Handler Input fill

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 
 
 const Register = () => {
+    const [email, setEmail] = useState("")
+    const handleEmailChange = (event)=>{
+        console.log(event.target.value);
+        setEmail(email.target.value);
+    }
+
+  
     return (
         <div>
             <h1>Register Form:</h1>
-            <form action="">
+            <form onChange={handleEmailChange} action="">
                 <input type="email" name="email" id="email" />
                 <br />
                 <input type="password" name="password"   id="password" />
